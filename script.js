a5d58a7cc7325d8eadd35977c82612fc2a7a2a6a
Ashley Gutierrez <ashgracie1994@icloud.com>
2022-09-08 13:42:46 -0700

    I made the wireframe.

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+Create a board
+Set dimensions of the board
+Player's place a token
+Board needs to be able to track tokens
+Determine who won or if it's a draw
+Reset the game
+*/
+
+
+class board{
+    constructor(rows, columns){
+        this.rows = rows;
+        this.columns = columns;
+    }
+}
