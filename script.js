d8a288aee789f2efe16f631e7f28d866c9e0c050
Ashley Gutierrez <ashgracie1994@icloud.com>
2022-09-11 13:12:28 -0700

    Created grid

--- script.js
+++ script.js
@@ -7,39 +7,86 @@ Determine who won or if it's a draw
 Reset the game
 */
 
+let board
 
 class Board{
-    arr = [];
     data = {}
     constructor(rows, cols){
         this.rows = rows;
         this.cols = cols;
-        this.data["0,0"]="Occupaied";
+        // this.data["0,0"]="Occupied";
+        this.generateHTML()
     }
     placeToken(token){
-        this.data["-2,-1"]=token;
-        // token row=2 col = 4
-        if(this.data["2,3"]==undefined){
-            // token row=2 col = 3
+        console.log("Place token ",token)
+        if(token.y == 4){
+            for(let i = 3; i > -1; i--){
+                if(this.data[token.x+","+i] == undefined){
+                    token.y = i
+                } else {
+                    break
+                }
+            }
+        } else if(token.x == 4){
+            for(let i = 3; i > -1; i--){
+                if(this.data[i + "," + token.y]== undefined){
+                    token.x = i 
+                } else {
+                    break
+                }
+            }
+        }else if(token.x == -4){
+            for(let i = 3; i > -1; i--){
+                if(this.data[-i + "," + token.y]== undefined){
+                    token.x = -i 
+                } else {
+                    break
+                }
+            }
+        }else if(token.y == -4){
+            for(let i = 3; i > -1; i--){
+                if(this.data[token.x+","+-i] == undefined){
+                    token.y = -i
+                } else {
+                    break
+                }
+            }
+            
         }
-        if(this.data["2,2"]==undefined){
-            // token row=2 col = 2
+        if((token.x != 4 && token.x!=-4) && (token.y != 4 && token.y!=-4)){
+            this.data[token.location()] = token
+            document.getElementById(token.location()).classList.add("token")
+        } else {
+            //error message
         }
-        if(this.data["2,1"]==undefined){
-            // token row=2 col = 1
-        }
-        if(this.data["2,0"]==undefined){
-            // token row=2 col = 0
-        }
-        for(i = 3; i > -1; i--){
-            if(this.data[token.row+","+i] == undefined){
-                token.col = i
-            } else {
-                break
+        console.log("Final location ",token, this.data)
+        this.check()
+    }
+    check(){
+        
+    }
+
+    generateHTML(){
+        let boardHTML=document.querySelector("#board")
+        for(let y = 4; y >= -4; y-- ){
+            for(let x = -4; x <= 4; x++){
+                let div = document.createElement("div");
+                div.id = x+","+y
+                div.innerHTML+=""+x+","+y+""
+                div.classList.add("grid-item");
+                if((y==4 || y==-4) && !(x==4 || x==-4) || (x==4 || x==-4) && !(y==4 || y==-4)) {
+                    let button = document.createElement("button");
+                    button.innerHTML="+";
+                    button.addEventListener('click', function() {
+                        board.placeToken(new Token(x,y))
+                    });
+                    div.append(button)
+                }
+
+                
+                boardHTML.append(div)
             }
         }
-        //data[token.location] = token
-
     }
 } 
 /*
@@ -59,19 +106,22 @@ class Board{
 
 
 class Token{
-    constructor(row, col, player){
-        this.row = row;
-        this.col = col;
+    constructor(x, y, player){
+        this.x = x;
+        this.y = y;
         this.player = player;
     }
     location(){
-       return row+","+col;
+       return this.x+","+this.y;
     }
 }
-let board = new Board(7, 7);
+
+
+board = new Board(7, 7);
 
 
 let token = new Token(2,4)
-board.placeToken(token);
+//board.placeToken(token);
 
-board.placeToken(new Token(2,4,1));
+//board.placeToken(new Token(2,4,1));
+
