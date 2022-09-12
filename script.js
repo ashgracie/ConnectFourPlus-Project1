f31899f4448939ccbba2350e69e2c2df1adf625c
Ashley Gutierrez <ashgracie1994@icloud.com>
2022-09-12 14:48:09 -0700

    Created interface for winners

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let board
 
 class Board{
     data = {}
+    currentPlayer = 1;
     constructor(rows, cols){
         this.rows = rows;
         this.cols = cols;
@@ -21,7 +22,7 @@ class Board{
         console.log("Place token ",token)
         if(token.y == 4){
             for(let i = 3; i > -1; i--){
-                if(this.data[token.x+","+i] == undefined){
+                if(this.data[loc(token.x,i)] == undefined){
                     token.y = i
                 } else {
                     break
@@ -29,7 +30,7 @@ class Board{
             }
         } else if(token.x == 4){
             for(let i = 3; i > -1; i--){
-                if(this.data[i + "," + token.y]== undefined){
+                if(this.data[loc(i,token.y)]== undefined){
                     token.x = i 
                 } else {
                     break
@@ -37,7 +38,7 @@ class Board{
             }
         }else if(token.x == -4){
             for(let i = 3; i > -1; i--){
-                if(this.data[-i + "," + token.y]== undefined){
+                if(this.data[loc(-i,token.y)]== undefined){
                     token.x = -i 
                 } else {
                     break
@@ -45,7 +46,7 @@ class Board{
             }
         }else if(token.y == -4){
             for(let i = 3; i > -1; i--){
-                if(this.data[token.x+","+-i] == undefined){
+                if(this.data[loc(token.x,-i)] == undefined){
                     token.y = -i
                 } else {
                     break
@@ -54,16 +55,58 @@ class Board{
             
         }
         if((token.x != 4 && token.x!=-4) && (token.y != 4 && token.y!=-4)){
+            token.player = this.currentPlayer
             this.data[token.location()] = token
+            this.check(token)
+
+            
+            
             document.getElementById(token.location()).classList.add("token")
+            document.getElementById(token.location()).innerHTML = this.currentPlayer==1 ? "❌":"⭕️"
+
+            if(this.currentPlayer == 1){
+                this.currentPlayer = 2
+            } else if(this.currentPlayer == 2) {
+                this.currentPlayer = 1
+            }
+            document.querySelectorAll(".grid-item button").forEach((button)=>{
+                button.innerHTML= this.currentPlayer==1 ? "❌":"⭕️"
+            })
         } else {
             //error message
         }
         console.log("Final location ",token, this.data)
-        this.check()
     }
-    check(){
-        
+    check(token){
+        let breakers = [false,false,false,false,false,false,false,false];
+        let counters = [0,0,0,0,0,0,0,0];
+        for(let s=1; s<=4; s++){
+            let locations = [];
+            locations.push(loc(token.x + s,token.y))
+            locations.push(loc(token.x - s,token.y))
+            locations.push(loc(token.x,token.y + s))
+            locations.push(loc(token.x,token.y - s))
+            locations.push(loc(token.x + s,token.y + s))
+            locations.push(loc(token.x - s,token.y - s))
+            locations.push(loc(token.x - s,token.y + s))
+            locations.push(loc(token.x + s,token.y - s))
+            locations.forEach((loc,idx)=>{
+                if(breakers[idx]){return}
+                if(this.data[loc]==undefined || this.data[loc].player!=token.player){
+                    breakers[idx] = true;
+                } else {
+                    counters[idx]++;
+                }
+            })
+            console.log(locations)
+            console.log(counters)
+        }
+        console.log(counters)
+        if(counters[0]+counters[1]>=3 || counters[2]+counters[3]>=3 || counters[4]+counters[5]>=3 || counters[6]+counters[7]>=3) {
+            alert("you win")
+
+        }
+
     }
 
     generateHTML(){
@@ -71,12 +114,14 @@ class Board{
         for(let y = 4; y >= -4; y-- ){
             for(let x = -4; x <= 4; x++){
                 let div = document.createElement("div");
-                div.id = x+","+y
-                div.innerHTML+=""+x+","+y+""
+                div.id = loc(x,y)
+                // div.innerHTML+=loc(x,y)
                 div.classList.add("grid-item");
                 if((y==4 || y==-4) && !(x==4 || x==-4) || (x==4 || x==-4) && !(y==4 || y==-4)) {
                     let button = document.createElement("button");
-                    button.innerHTML="+";
+                    button.innerHTML="⭕️";
+                    button.innerHTML="❌";
+
                     button.addEventListener('click', function() {
                         board.placeToken(new Token(x,y))
                     });
@@ -112,8 +157,11 @@ class Token{
         this.player = player;
     }
     location(){
-       return this.x+","+this.y;
+       return loc(this.x,this.y)
     }
+}
+function loc(x,y) {
+    return x+","+y;
 }
 
 
